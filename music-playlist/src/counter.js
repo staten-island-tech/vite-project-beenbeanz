a5e4d6785985881199122e772de4da104bcd6d37
Hailey Lan <haileylan404@gmail.com>
2025-10-21 14:02:28 -0400

    tuesday skool

--- music-playlist/src/counter.js
+++ music-playlist/src/counter.js
@@ -1,4 +1,20 @@
-export const songsArr = [];
+export const songsArr = [
+  {name: "The 30th", 
+    artist: "Billie Eilish", 
+    album: "Happier Than Ever",
+    length: "3:15"
+  },
+  {name: "Bittersuite", 
+    artist: "Billie Eilish", 
+    album: "Hit me Hard and Soft",
+    length: "4:58"
+  },
+  {name: "i love you", 
+    artist: "Billie Eilish", 
+    album: "WHEN WE ALL FALL ASLEEP, WHERE DO WE GO",
+    length: "4:51"
+  },
+];
 
 
 export function setupCounter(element) {
@@ -10,3 +26,21 @@ export function setupCounter(element) {
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
 }
+
+export function renderSongs(arr, element){
+  arr.forEach(song => {
+    const html = `
+      <div class="songCard">
+          <button class="playPauseBtn">▶</button>
+          <div class="songTexts">
+            <p class="songTitle">${song.name}</p>
+            <p class="songArtist">${song.artist}</p>
+            <p class="songAlbum">${song.album}</p>
+            <p class="songDuration">${song.length}</p>
+          </div>
+          <img src="src/hte.png" alt="Happier Than Ever Album Cover">
+      </div>
+    `;
+    element.innerHTML += html;
+});
+} 

02e193d592ccdfd8ef4a1026de63d92ee3c2da6f
Hailey Lan <haileylan404@gmail.com>
2025-12-09 13:55:16 -0500

    fwrwr

--- music-playlist/src/pet.js
+++ music-playlist/src/pet.js
@@ -190,12 +190,12 @@ export function decreaseHappiness(){
 
 export function playGif(){
     const petImg = document.querySelector('.selectedPet');
-    petImg.src = 'public/' + petImg.dataset.animal + '.gif';
+    petImg.src = '/' + petImg.dataset.animal + '.gif';
 }
 
 export function stopGif(){
     const petImg = document.querySelector('.selectedPet');
-    petImg.src = 'public/' + petImg.dataset.animal + '.png';
+    petImg.src = '/' + petImg.dataset.animal + '.png';
 }
 /*  one min of song -> happy++
     one song added -> hunger++
